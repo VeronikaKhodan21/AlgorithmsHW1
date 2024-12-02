7d5eabeb6ecc7ae11b4a1863c85d59ada3a47e8e
VeronikaKhodan21 <146882332+VeronikaKhodan21@users.noreply.github.com>
2024-12-02 14:39:30 +0300

    The job is done

--- HeapSorts.java
+++ HeapSorts.java
@@ -1,3 +1,31 @@
 public class HeapSorts {
+    public static void sort(int[] arr) {
+        for (int i = arr.length/ 2 -1 ; i >= 0; i--) {
+            heapify(arr, arr.length, i);
+        }
+        for (int i = arr.length -1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+        
+    }
+    private static  void heapify(int[] arr, int heapSize, int rootI){
+        int lar = rootI;
+        int leftChild = 2* rootI +1;
+        int righChild = 2* rootI + 2;
+        if(leftChild < heapSize && arr[leftChild] > arr[lar]){
+            lar = leftChild;
+        }
+        if(righChild < heapSize && arr[righChild] > arr[lar]){
+            lar = righChild;
+        }if(lar != rootI){
+            int tmp = arr[rootI];
+            arr[rootI] = arr[lar];
+            arr[lar] = tmp;
+            heapify(arr, heapSize, lar);
+        }
+    }
 
 }
